00e056d6e3df47199d651b2ffd937f52d6dacb07
OA-1234 <oabdel2003@gmail.com>
2024-10-23 06:41:53 +0800

    New listen

--- listen.py
+++ listen.py
@@ -16,131 +16,141 @@ class Encoder(object):
         self.encoder = DigitalInputDevice(pin)
         self.encoder.when_activated = self._increment
         self.encoder.when_deactivated = self._increment
-
+    
     def reset(self):
         self._value = 0
-
+    
     def _increment(self):
         self._value += 1
-
+        
     @property
     def value(self):
         return self._value
-
+        
 
 # main function to control the robot wheels
 def move_robot():
-    global use_pid, left_speed, right_speed
+    global use_pid, left_speed, right_speed,time_to_drive
     flag_new_pid_cycle = True
     while True:
         ### if not using pid, just move the wheels as commanded
         if not use_pid:
-            pibot.value = (left_speed, right_speed)
-
+            pibot.value = (left_speed, right_speed)          
+        
         ### with pid, left wheel is set as reference, and right wheel will try to match the encoder counter of left wheel
         ### pid only runs when robot moves forward or backward. Turning does not use pid
         else:
-            if (motion == "stop") or (motion == "turning"):
-                pibot.value = (left_speed, right_speed)
+            if (motion == 'autostop') or (motion == 'autoturning'):
+                pibot.value = (left_speed, right_speed) 
+                time.sleep(time_to_drive)
+                pibot.value = (0,0)
                 left_encoder.reset()
                 right_encoder.reset()
                 flag_new_pid_cycle = True
+            elif (motion == 'stop') or (motion == 'turning'):
+                pibot.value = (left_speed, right_speed) 
+
+                left_encoder.reset()
+                right_encoder.reset()
+                flag_new_pid_cycle = True          
             else:
                 left_speed, right_speed = abs(left_speed), abs(right_speed)
                 if flag_new_pid_cycle:
-                    pid_right = PID(
-                        kp,
-                        ki,
-                        kd,
-                        setpoint=left_encoder.value,
-                        output_limits=(0, 1),
-                        starting_output=right_speed,
-                    )
+                    pid_right = PID(kp, ki, kd, setpoint=left_encoder.value, output_limits=(0,1), starting_output=right_speed)
                     flag_new_pid_cycle = False
                 pid_right.setpoint = left_encoder.value
                 right_speed = pid_right(right_encoder.value)
-                if motion == "forward":
+                if motion == 'forward': 
                     pibot.value = (left_speed, right_speed)
-                else:
+                elif motion == 'backward': 
                     pibot.value = (-left_speed, -right_speed)
+                elif motion == 'autoforward':
+                    pibot.value = (left_speed,right_speed)
+                    time.sleep(time_to_drive)
+                    pibot.value = (0,0)
+                elif motion == 'backward': 
+                    pibot.value = (-left_speed, -right_speed)
+                    time.sleep(time_to_drive)
+                    pibot.value = (0,0)
+
                 # print('Value', left_encoder.value, right_encoder.value)
+
                 # print('Speed', left_speed, right_speed)
         time.sleep(0.005)
-
-
+    
+    
 # Receive confirmation whether to use pid or not to control the wheels (forward & backward)
-@app.route("/pid")
+@app.route('/pid')
 def set_pid():
     global use_pid, kp, ki, kd
-    use_pid = int(request.args.get("use_pid"))
-    left_value = left_encoder.value
-    right_value = right_encoder.value
-    # Return the values as a JSON response
-    # return {"left_encoder": left_value, "right_encoder": right_value}
+    use_pid = int(request.args.get('use_pid'))
     if use_pid:
-        kp, ki, kd = (
-            float(request.args.get("kp")),
-            float(request.args.get("ki")),
-            float(request.args.get("kd")),
-        )
+        kp, ki, kd = float(request.args.get('kp')), float(request.args.get('ki')), float(request.args.get('kd'))
         return "Using PID"
     else:
         return "Not using PID"
-
-
+    
 # Receive a request to capture and send a snapshot of the picamera
-@app.route("/image")
+@app.route('/image')
 def capture_image():
     stream = io.BytesIO()
-    picam2.capture_file(stream, format="jpeg")
+    picam2.capture_file(stream, format='jpeg')
     stream.seek(0)
-    return Response(stream, mimetype="image/jpeg")
+    return Response(stream, mimetype='image/jpeg')
+    
 
-
-# Receive command to move the pibot
-@app.route("/move")
+ # Receive command to move the pibot
+@app.route('/move')
 def move():
     global left_speed, right_speed, motion
-    left_speed, right_speed = float(request.args.get("left_speed")), float(
-        request.args.get("right_speed")
-    )
-
-    if left_speed == 0 and right_speed == 0:
-        motion = "stop"
-    elif left_speed != right_speed:
-        motion = "turning"
-    elif left_speed > 0 and right_speed > 0:
-        motion = "forward"
-    elif left_speed < 0 and right_speed < 0:
-        motion = "backward"
+    left_speed, right_speed = float(request.args.get('left_speed')), float(request.args.get('right_speed'))
+    
+    if (left_speed == 0 and right_speed == 0):
+        motion = 'stop'
+    elif (left_speed != right_speed ):
+        motion = 'turning'
+    elif (left_speed > 0 and right_speed > 0):
+        motion = 'forward'
+    elif (left_speed < 0 and right_speed < 0):
+        motion = 'backward'
+    return motion
+    
+    # if 'time' in request.args:
+@app.route('/automove')
+def automove():
+    global left_speed, right_speed, motion,time_to_drive
+    left_speed, right_speed = float(request.args.get('left_speed')), float(request.args.get('right_speed'))
+    time_to_drive = float(request.args.get('drive_time'))
+    
+    if (left_speed == 0 and right_speed == 0):
+        motion = 'autostop'
+    elif (left_speed != right_speed ):
+        motion = 'autoturning'
+    elif (left_speed > 0 and right_speed > 0):
+        motion = 'autoforward'
+    elif (left_speed < 0 and right_speed < 0):
+        motion = 'autobackward'
     return motion
 
-    # if 'time' in request.args:
 
-
-@app.route("/encoders")
-def get_encoder_values():
-    left_value = left_encoder.value
-    right_value = right_encoder.value
-    # Return the values as a JSON response
-    return {"left_encoder": left_value, "right_encoder": right_value}
+@app.route('/encoders')
+def getEncoderValues():
+    global left_encoder,right_encoder
+    return left_encoder.value,right_encoder.value
 
 
 # Constants
-in1 = 17  # may have to change this
-in2 = 27  # may have to change this
+in1 = 17 # may have to change this
+in2 = 27 # may have to change this
 ena = 18
-in3 = 23  # may have to change this
-in4 = 24  # may have to change this
+in3 = 23 # may have to change this
+in4 = 24 # may have to change this
 enb = 25
 enc_a = 26
 enc_b = 16
 
 # Initialize robot and encoders
-pibot = Robot(
-    right=Motor(forward=in1, backward=in2, enable=ena),
-    left=Motor(forward=in3, backward=in4, enable=enb),
-)
+pibot = Robot(right=Motor(forward=in1, backward=in2, enable=ena), left=Motor(forward=in3, backward=in4, enable=enb))
 
 # Initialize PID controllers for wheels
 left_encoder = Encoder(enc_a)
@@ -150,21 +160,19 @@ kp = 0
 ki = 0
 kd = 0
 left_speed, right_speed = 0, 0
-motion = ""
+motion = ''
+time_to_drive = 0
 
 # Initialize the PiCamera
 picam2 = Picamera2()
-config = picam2.create_preview_configuration(lores={"size": (640, 480)})
+config = picam2.create_preview_configuration(lores={"size": (640,480)})
 picam2.configure(config)
 picam2.start()
 time.sleep(2)
 
-
 # Initialize flask
 def run_flask():
-    app.run(host="0.0.0.0", port=5000)
-
-
+    app.run(host='0.0.0.0', port=5000)
 flask_thread = threading.Thread(target=run_flask)
 flask_thread.daemon = True
 flask_thread.start()
